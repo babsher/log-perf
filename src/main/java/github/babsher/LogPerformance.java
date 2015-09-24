5f3b9627b542426ef8cab884e5d2bd3d98409bc8
Bryan Absher <bryan.absher@gmail.com>
2015-09-24 14:23:58 -0400

    added configuration for log4j

--- src/main/java/github/babsher/LogPerformance.java
+++ src/main/java/github/babsher/LogPerformance.java
@@ -1,11 +1,14 @@
 package github.babsher;
 
+import javax.inject.Singleton;
+
 import dagger.Component;
 import github.babsher.config.ConfigProvider;
 import github.babsher.job.log4j.Log4jModule;
 import github.babsher.job.sysout.SysOutModule;
 
-@Component(modules = {ConfigProvider.class, SysOutModule.class})
+@Singleton
+@Component(modules = {ConfigProvider.class, Log4jModule.class})
 public interface LogPerformance {
   TestRun testRun();
   App app();
--- src/main/java/github/babsher/job/log4j/Log4jModule.java
+++ src/main/java/github/babsher/job/log4j/Log4jModule.java
@@ -1,6 +1,11 @@
 package github.babsher.job.log4j;
 
+import org.apache.log4j.Appender;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 
 import javax.inject.Singleton;
 
@@ -19,8 +24,30 @@ public class Log4jModule {
 
   @Provides
   @Singleton
-  public Logger logger() {
+  public Logger logger(Config config, Appender appender) {
+    BasicConfigurator.configure(appender);
+    Logger l = Logger.getLogger(Log4jJob.class);
+    switch (config.getLevel()) {
+      case Debug: l.setLevel(Level.DEBUG); break;
+      case Info: l.setLevel(Level.INFO); break;
+      case Trace: l.setLevel(Level.TRACE); break;
+      case Warn: l.setLevel(Level.WARN); break;
+      case Error: l.setLevel(Level.ERROR); break;
+    }
+    return l;
+  }
 
-    return Logger.getLogger(Log4jJob.class);
+  @Provides
+  @Singleton
+  public Appender appender() {
+    ConsoleAppender c = new ConsoleAppender();
+
+    String PATTERN = "%d [%p|%c|%C{1}] %m%n";
+    c.setLayout(new PatternLayout(PATTERN));
+    c.activateOptions();
+    //add appender to any Logger (here is root)
+    Logger.getRootLogger().addAppender(c);
+
+    return c;
   }
 }
